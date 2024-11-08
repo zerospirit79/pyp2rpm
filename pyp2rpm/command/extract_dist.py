87b7ea5300ec8362a13077f95c25035f1f5eb403
Pavel Shilov <61824830+zerospirit79@users.noreply.github.com>
2024-11-08 17:10:09 +0300

    Update extract_dist.py

--- pyp2rpm/command/extract_dist.py
+++ pyp2rpm/command/extract_dist.py
@@ -1,74 +1,50 @@
 import sys
 import json
-from setuptools import find_packages
+from setuptools import setup, find_packages  # Импорт setuptools
 
 
-class extract_dist:
-    """Custom distutils command to extract metadata form setup function."""
+class extract_dist:  # Удален наследование от Command
+    """Custom function to extract metadata."""
     class_metadata = None
 
-    def __init__(self, *args, **kwargs):
-        """Metadata dictionary is created, all the metadata attributes,
-        that were not found are set to default empty values. Checks of data
-        types are performed.
-        """
-
+    def __init__(self, metadata_from_setup):
+        """Metadata dictionary is created."""
         self.metadata = {}
         self.fill_metadata(metadata_from_setup)
 
-   def fill_metadata(self, metadata):
-      self.metadata = {} 
-        for attr in ['setup_requires', 'tests_require', 'install_requires',
-                     'packages', 'py_modules', 'scripts']:
-            self.metadata[attr] = to_list(getattr(self.distribution, attr, []))
+    def fill_metadata(self, metadata):
+      self.metadata = {}
+      for attr in ['setup_requires', 'tests_require', 'install_requires',
+                   'packages', 'py_modules', 'scripts']:
+          self.metadata[attr] = to_list(metadata.get(attr, []))
 
-        try:
-            for k, v in getattr(
-                    self.distribution, 'extras_require', {}).items():
-                if k in ['test, docs', 'doc', 'dev']:
-                    attr = 'setup_requires'
-                else:
-                    attr = 'install_requires'
-                self.metadata[attr] += to_list(v)
-        except (AttributeError, ValueError):
-            # extras require are skipped in case of wrong data format
-            # can't log here, because this file is executed in a subprocess
-            pass
+      try:
+          for k, v in metadata.get('extras_require', {}).items():
+              if k in ['test, docs', 'doc', 'dev']:
+                  attr = 'setup_requires'
+              else:
+                  attr = 'install_requires'
+              self.metadata[attr] += to_list(v)
+      except (AttributeError, ValueError):
+          pass # extras_require are skipped in case of wrong data format
 
-        for attr in ['url', 'long_description', 'description', 'license']:
-            self.metadata[attr] = to_str(
-                getattr(self.distribution.metadata, attr, None))
+      for attr in ['url', 'long_description', 'description', 'license']:
+          self.metadata[attr] = to_str(metadata.get(attr))
 
-        self.metadata['classifiers'] = to_list(
-            getattr(self.distribution.metadata, 'classifiers', []))
+      self.metadata['classifiers'] = to_list(metadata.get('classifiers', []))
 
-        if isinstance(getattr(self.distribution, "entry_points", None), dict):
-            self.metadata['entry_points'] = self.distribution.entry_points
-        else:
-            self.metadata['entry_points'] = None
+      self.metadata['entry_points'] = metadata.get('entry_points')
 
-        self.metadata['test_suite'] = getattr(
-            self.distribution, "test_suite", None) is not None
+      self.metadata['test_suite'] = metadata.get("test_suite") is not None
 
-    def initialize_options(self):
-        """Sets default value of the stdout option."""
-        self.stdout = False
-
-    def finalize_options(self):
-        """Abstract method of Command class have to be overridden."""
-        pass
 
     def run(self):
-        """Sends extracted metadata in json format to stdout if stdout
-        option is specified, assigns metadata dictionary to class_metadata
-        variable otherwise.
-        """
+        """Sends extracted metadata in json format to stdout."""
         if self.stdout:
             sys.stdout.write("extracted json data:\n" + json.dumps(
                 self.metadata, default=to_str) + "\n")
         else:
             extract_dist.class_metadata = self.metadata
-
 
 def to_list(var):
     """Checks if given value is a list, tries to convert, if it is not."""
@@ -90,3 +66,8 @@ def to_str(var):
         return str(var)
     except TypeError:
         raise ValueError("{} cannot be converted to string.".format(var))
+
+# Example of how to use the class
+# metadata_dict = {"name": "mypackage", "version": "0.1.0", "packages": ["mypackage"], "install_requires": ["requests"]}
+# extractor = extract_dist(metadata_dict)
+# extractor.run()
